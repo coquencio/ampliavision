dc9abba5e649cb337e0cd16ddf5730c770cecce6
Jorge Dominguez <jorge.dm97@gmail.com>
2020-04-02 22:07:50 -0600

    added-examen-controller

--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,14 @@ from src.Controllers.UserManagerController import UserController
 from src.Controllers.empresaController import EmpresaController
 from src.Controllers.beneficiarioController import BeneficiarioController
 from src.Controllers.ojosController import OjosController
+from src.Controllers.examenController import ExamenController
 
 app = Flask(__name__)
 app.register_blueprint(UserController)
 app.register_blueprint(EmpresaController)
 app.register_blueprint(BeneficiarioController)
 app.register_blueprint(OjosController)
+app.register_blueprint(ExamenController)
 
 
 @app.route('/')
