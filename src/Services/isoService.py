a8ea748e68cff7e3289f1d1f5d020fce05530020
Jorge Dominguez <jorge.dm97@gmail.com>
2020-04-06 23:22:48 -0500

    iso-services-and-controller

---
+++ src/Services/isoService.py
@@ -0,0 +1,32 @@
+from src.Core.constants import SpCasosIso
+from src.Helpers.sql import MySqlHelper
+from src.Helpers.serializer import serialize_data_set
+from src.Services.beneficiariosService import BeneficiariosService
+
+
+class IsoService:
+    def __init__(self):
+        self.__sql_helper = MySqlHelper()
+        self.__beneficiario_service = BeneficiariosService()
+
+    def get_active_ones(self):
+        data = self.__sql_helper.sp_get(SpCasosIso.Get_active)
+        return serialize_data_set(data, "Casos")
+
+    def get_by_beneficiario(self, beneficiario_id):
+        if not isinstance(beneficiario_id, int):
+            raise ValueError("Invalid id")
+        args = (str(beneficiario_id), )
+        data = self.__sql_helper.sp_get(SpCasosIso.Get_by_beneficiario, args)
+        if not data:
+            return False
+        return serialize_data_set(data, "Casos")
+
+    def create_relation(self, beneficiario_id, caso_id):
+        if not isinstance(beneficiario_id, int) or not isinstance(caso_id, int):
+            raise ValueError("Invalid arguments")
+        if not self.__beneficiario_service.validate_existance(beneficiario_id):
+            return False
+        args = (str(beneficiario_id), str(caso_id))
+        self.__sql_helper.sp_set(SpCasosIso.Create_relation, args)
+        return True
