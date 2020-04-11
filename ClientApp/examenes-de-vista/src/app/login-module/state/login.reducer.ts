6cab748dcb39966eb5a41ce604b4c8c28bf701ce
Jorge Dominguez <jorge.dm97@gmail.com>
2020-04-11 13:02:17 -0500

    state-management

---
+++ ClientApp/examenes-de-vista/src/app/login-module/state/login.reducer.ts
@@ -0,0 +1,12 @@
+export function reducer (state, action){
+
+    switch (action.type){
+        case "SET_TOKEN":
+            return {
+                ...state,
+                token: action.payload,
+            };
+        default:
+            return state;
+    }
+}
