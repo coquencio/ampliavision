a5f399dbe3413954e8fd03c4bed8460023036bc3
Jorge Dominguez <jorge.dm97@gmail.com>
2020-04-10 22:22:14 -0500

    enabled-CORS

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     name="AmpliaVision",
     version="1.0.0",
     description="Api controllers for database administration",
-    install_requires=["flask", "pymysql", "cryptography"],
+    install_requires=["flask", "pymysql", "cryptography", "flask-cors"],
 )
