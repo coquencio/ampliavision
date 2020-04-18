ab96d90dc06d6172a4cfb743395a143207ff3d46
Jorge Dominguez <jorge.dm97@gmail.com>
2020-04-18 18:45:48 -0500

    additional-components

---
+++ ClientApp/examenes-de-vista/src/app/components/defectos-visuales/defectos-visuales.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-defectos-visuales',
+  templateUrl: './defectos-visuales.component.html',
+  styleUrls: ['./defectos-visuales.component.css']
+})
+export class DefectosVisualesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
