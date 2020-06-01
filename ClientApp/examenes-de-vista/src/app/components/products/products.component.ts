76aed0adb1262f90213d1759710f33740a7fa930
Jorge Dominguez <jorge.dm97@gmail.com>
2020-05-31 19:41:19 -0600

    Item registration

--- ClientApp/examenes-de-vista/src/app/components/products/products.component.ts
+++ ClientApp/examenes-de-vista/src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { IMarca, IColor, ITamanio, IModelo, IMaterial, IProteccion, ILente, IGeneral } from 'src/app/Interfaces/generalInterface';
 import { GeneralService } from 'src/app/services/general/general.service';
 
@@ -12,6 +12,7 @@ export class ProductsComponent implements OnInit {
   constructor(
     private generalService: GeneralService
     ) { }
+  @ViewChild('closeModal') private closeModal: ElementRef;
   marcas: IMarca[];
   colores: IColor[];
   tamanios: ITamanio[];
@@ -21,32 +22,64 @@ export class ProductsComponent implements OnInit {
   lentes: ILente[];
   currentName: string;
   inTable: IGeneral[]; 
+  descripcion: string;
 
   ngOnInit(): void {
-    this.generalService.Get('marcas').subscribe(
-      r => this.marcas = r.marcas
-    );
-    this.generalService.Get('colores').subscribe(
-      r => this.colores = r.colores
-    );
-    this.generalService.Get('tamanios').subscribe(
-      r => this.tamanios = r.tamanios
-    );
-    this.generalService.Get('modelos').subscribe(
-      r => this.modelos = r.modelos
-    );
-    this.generalService.Get('materiales').subscribe(
-      r => this.materiales= r.materiales
-    );
-    this.generalService.Get('protecciones').subscribe(
-      r => this.protecciones = r.protecciones
-    );
-    this.generalService.Get('lentes').subscribe(
-      r => this.lentes = r.lentes
-    );
+    this.getGeneralItems();
   }
 
-
+  private getGeneralItems(){
+    this.generalService.Get('marcas').subscribe(
+      r => {
+        this.marcas = r.marcas;
+        if(this.currentName === 'marcas'){
+          this.SetCurrentObject('marcas')
+        } 
+      }
+    );
+    this.generalService.Get('colores').subscribe(
+      r =>{ this.colores = r.colores;
+        if(this.currentName === 'colores'){
+          this.SetCurrentObject('colores')
+        }
+      }
+    );
+    this.generalService.Get('tamanios').subscribe(
+      r => {this.tamanios = r.tamanios;
+        if(this.currentName === 'tamanios'){
+          this.SetCurrentObject('tamanios')
+        }
+      }
+    );
+    this.generalService.Get('modelos').subscribe(
+      r => {
+        this.modelos = r.modelos;
+        if(this.currentName === 'modelos'){
+          this.SetCurrentObject('modelos')
+        }
+      }
+    );
+    this.generalService.Get('materiales').subscribe(
+      r => {this.materiales= r.materiales; 
+        if(this.currentName === 'materiales'){
+          this.SetCurrentObject('materiales')
+        }}
+    );
+    this.generalService.Get('protecciones').subscribe(
+      r => {this.protecciones = r.protecciones; 
+        if(this.currentName === 'protecciones'){
+          this.SetCurrentObject('protecciones')
+        }
+      }
+    );
+    this.generalService.Get('lentes').subscribe(
+      r =>{ this.lentes = r.lentes;
+        if(this.currentName === 'lentes'){
+          this.SetCurrentObject('lentes')
+        } }
+    );
+  }
+  
   SetCurrentObject(name: string){
     this.currentName = name;
     this.inTable = this.getType(name);
@@ -69,6 +102,21 @@ export class ProductsComponent implements OnInit {
       case ('lentes'):
         return this.lentes.map(m=> {return <IGeneral>{ID : m.LenteID, Descripcion: m.Descripcion }});;
     }
+  }
+
+  Create(){
+    if (this.descripcion === '' || this.descripcion === undefined){
+      window.alert('Por favor introduce una descripción');
+      return;
+    }
+    this.generalService.Create(this.currentName, this.descripcion).subscribe(
+      ()=> {
+        this.getGeneralItems();
+        this.descripcion = '';
+        window.alert('Registro añadido satisfactoriamente en ' + this.currentName);
+        this.closeModal.nativeElement.click();
+      }
+    );
   }
   
 }
--- ClientApp/examenes-de-vista/src/app/services/general/general.service.ts
+++ ClientApp/examenes-de-vista/src/app/services/general/general.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TokenService } from '../token/token.service';
 import { Observable } from 'rxjs';
 import { AppSettings } from 'src/app/core/constants';
@@ -15,5 +15,9 @@ export class GeneralService {
   ) { }
   Get (entity:string): Observable<any>{
     return this.httpclient.get<any>(AppSettings.BASE_ADDRESS + entity + '?token=' + this.tokenService.GetToken());    
+  }
+  Create(entity: string, description: string): Observable<any>{
+    const headers = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
+    return this.httpclient.post(AppSettings.BASE_ADDRESS + entity + '/create/' + description + '?token=' + this.tokenService.GetToken(),{},{headers: headers, responseType:'text'});
   }
 }
