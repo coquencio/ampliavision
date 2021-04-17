b13e743f5ba5fa70a0a8bf57e26899fc061a839a
Jorge Dominguez <jorge.dm97@gmail.com>
2021-04-17 12:13:50 -0600

    Uppercase prod issue

--- Database/Enhancement2021.sql
+++ Database/Enhancement2021.sql
@@ -1,6 +1,6 @@
 
-insert into enfermedadesvisuales (Descripcion) values ('Miopía, Hipermetropía y Astigmatismo');
-insert into enfermedadesvisuales (Descripcion) values ('Miopía, Hipermetropía y Presbicia');
-insert into enfermedadesvisuales (Descripcion) values ('Miopía, Hipermetropía, Astigmatismo y Presbicia');
-insert into enfermedadesvisuales (Descripcion) values ('Oftalmologo');
+insert into EnfermedadesVisuales(Descripcion) values ('Miopía, Hipermetropía y Astigmatismo');
+insert into EnfermedadesVisuales (Descripcion) values ('Miopía, Hipermetropía y Presbicia');
+insert into EnfermedadesVisuales (Descripcion) values ('Miopía, Hipermetropía, Astigmatismo y Presbicia');
+insert into EnfermedadesVisuales (Descripcion) values ('Oftalmologo');
         
