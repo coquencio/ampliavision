bd5dcbb20d347eac203fba89b835f488cf3e1cbe
Jorge Dominguez <jorge.dm97@gmail.com>
2021-04-17 12:11:37 -0600

    New diseases combinations

---
+++ Database/Enhancement2021.sql
@@ -0,0 +1,6 @@
+
+insert into enfermedadesvisuales (Descripcion) values ('Miopía, Hipermetropía y Astigmatismo');
+insert into enfermedadesvisuales (Descripcion) values ('Miopía, Hipermetropía y Presbicia');
+insert into enfermedadesvisuales (Descripcion) values ('Miopía, Hipermetropía, Astigmatismo y Presbicia');
+insert into enfermedadesvisuales (Descripcion) values ('Oftalmologo');
+        
